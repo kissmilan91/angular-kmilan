df2c446e9b7b09aa1a7556cf866eb70d6b889c2f
kissmilan91 <kissmilan91@gmail.com>
2021-11-19 22:16:53 +0100

    Változtatások

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-kmilan'),
+      dir: require('path').join(__dirname, './coverage/vizsgaremek'),
       subdir: '.',
       reporters: [
         { type: 'html' },
