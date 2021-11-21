1c7df2b2ea7cb1cb84239d35abd0bce608c099dc
kissmilan91 <kissmilan91@gmail.com>
2021-11-21 12:39:55 +0100

    Komponensek_létrehozása

--- bootstrapprojekt/src/app/app.module.ts
+++ bootstrapprojekt/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FejlecComponent } from './fejlec/fejlec.component';
+import { MenuComponent } from './menu/menu.component';
+import { LablecComponent } from './lablec/lablec.component';
+import { KezdolapComponent } from './kezdolap/kezdolap.component';
+import { KapcsolatComponent } from './kapcsolat/kapcsolat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FejlecComponent,
+    MenuComponent,
+    LablecComponent,
+    KezdolapComponent,
+    KapcsolatComponent
   ],
   imports: [
     BrowserModule,
---
+++ bootstrapprojekt/src/app/kezdolap/kezdolap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kezdolap',
+  templateUrl: './kezdolap.component.html',
+  styleUrls: ['./kezdolap.component.css']
+})
+export class KezdolapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
