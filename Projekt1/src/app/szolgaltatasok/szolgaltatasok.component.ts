d40411884a795f0c544bbc385cdd63c7e006b914
kissmilan91 <kissmilan91@gmail.com>
2021-11-21 12:02:22 +0100

    Update_2021_11_21

---
+++ Projekt1/src/app/szolgaltatasok/szolgaltatasok.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-szolgaltatasok',
+  templateUrl: './szolgaltatasok.component.html',
+  styleUrls: ['./szolgaltatasok.component.css']
+})
+export class SzolgaltatasokComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
