0d2081845c6c21d7276578841b5cf8509a2128b9
kissmilan91 <kissmilan91@gmail.com>
2021-11-21 18:53:56 +0100

    Frontend_Kezdőlap_CSS_HTML_JS

--- bootstrapprojekt/src/app/app-routing.module.ts
+++ bootstrapprojekt/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FejlecComponent } from './fejlec/fejlec.component';
+import { KapcsolatComponent } from './kapcsolat/kapcsolat.component';
+import { KezdolapComponent } from './kezdolap/kezdolap.component';
+import { LablecComponent } from './lablec/lablec.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  "", pathMatch:  "full",redirectTo:  "kezdolap"},
+  {path: "fejlec",component: FejlecComponent},
+  {path: "kapcsolat",component: KapcsolatComponent},
+  {path: "kezdolap",component: KezdolapComponent},
+  {path: "lablec",component: LablecComponent},
+  {path: "menu",component: MenuComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
