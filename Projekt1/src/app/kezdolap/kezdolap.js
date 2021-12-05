9da169f2197ee1ebdfe71917811c9726c6160743
kissmilan91 <kissmilan91@gmail.com>
2021-12-05 21:27:35 +0100

    Update

---
+++ Projekt1/src/app/kezdolap/kezdolap.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+
+
+    $('.owl-carousel').slick({
+    infinite: true,
+    centerMode: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    
+    
+    });
+    
+    });
